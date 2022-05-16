a3dc9c3497a614e0ba82dfaab6562e448d46c2c9
Teemu Rytsölä <teemu.rytsola@yonoton.com>
2022-05-16 21:01:13 +0300

    recreate svguri bug

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {SvgUri} from "react-native-svg";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <SvgUri width="60" height="40" uri="https://hatscripts.github.io/circle-flags/flags/gb.svg"/>
       <StatusBar style="auto" />
     </View>
   );
